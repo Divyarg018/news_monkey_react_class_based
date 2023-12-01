33664cd410e7a8308c1c243b04609c240f48a736
Divya R <kwrdivyaraj100@gmail.com>
2023-12-01 16:09:08 +0530

    Rendering the Image inside Cards in NewsItem.jsx

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        const { title, description } = this.props;
+        const { title, description, imageUrl } = this.props;
         return (
             <div className='my-3'>
-                <div className="card" style={{ width: '18rem'}}>
-                    <img src="..." className="card-img-top" alt="..." />
+                <div className="card" style={{ width: '18rem' }}>
+                    <img src={imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <a href="/newsdetail" className="btn btn-sm btn-primary">Read More</a>
+                        <a rel="noreferrer" href="/newsdetail" target='_blank' className="btn btn-sm btn-primary">Read More</a>
                     </div>
                 </div>
             </div>
