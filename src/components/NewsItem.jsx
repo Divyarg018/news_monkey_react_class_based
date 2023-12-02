0ba2a0ad3408cbcc8d49f5398dcce85033edaa2e
Divya R <kwrdivyaraj100@gmail.com>
2023-12-02 16:38:53 +0530

    Using Author & Date

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        const { title, description, imageUrl } = this.props;
+        const { title, description, imageUrl, newsURL, author, date, source } = this.props;
         return (
             <div className='my-3'>
                 <div className="card" style={{ width: '18rem' }}>
@@ -10,7 +10,12 @@ export class NewsItem extends Component {
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <a rel="noreferrer" href="/newsdetail" target='_blank' className="btn btn-sm btn-primary">Read More</a>
+                        <p className='card-text'>
+                            <small className='text-muted'>
+                                By {author ? author : "Unknown"} on {new Date(date).toDateString()}
+                            </small>
+                        </p>
+                        <a rel="noreferrer" href={newsURL} target='_blank' className="btn btn-sm btn-primary">Read More</a>
                     </div>
                 </div>
             </div>
